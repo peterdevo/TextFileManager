fe16211f3d2712fc4b053054836be260fd054119
Cazper Hjortstam Sihvonen <54894732+cazhjo@users.noreply.github.com>
2020-04-02 10:54:13 +0200

    Update FileManager.cs

--- FileManagerLibrary/FileManager.cs
+++ FileManagerLibrary/FileManager.cs
@@ -39,7 +39,10 @@ namespace FileManagerLibrary
 
         private static List<string> SplitText(string text)
         {
-            return text.Split(' ').Select(x => x.Trim(',', '.', '-', '?', '!')).ToList();
+            return text.Split(
+                new[] { ' ', ',', '.', '-', '?', '!', '\n', '\r' },
+                StringSplitOptions.RemoveEmptyEntries
+                ).ToList();
         }
     }
 }
